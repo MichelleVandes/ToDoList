991fba904ccf373324e48e0c7adc9e658422e0fa
MichelleVandes <m.vandeschrick@codeur.online>
2022-03-04 15:29:47 +0000

    Fonctionnel mais moche :)

--- script2.js
+++ script2.js
@@ -1,8 +1,11 @@
 // Initialisation liste :
 const toDoEntry = document.getElementById("toDoEntry");
 let increment = 0;
+let id_Svg, newLine, newDiv, newButton;
 
-document.getElementById("modifie_Tache").disabled =true;
+document.getElementById("maj_Line").disabled =true;
+document.getElementById("del_Line").disabled = true;
+
 
 // Ajouter une nouvelle ligne de tache to do :
 function ajouteTache() {
@@ -19,10 +22,10 @@ function ajouteTache() {
   // Récupération de la tache
   console.log("Valeur Entrée : ", toDoEntry.value);
   // Création d'une nouvelle ligne
-  const newDiv = document.createElement("div"); // ajouter la classe
+  newDiv = document.createElement("div"); // ajouter la classe
   newDiv.classList += "to_Do";
   // Ajout element de liste; et l'associer à la div
-  const newLine = document.createElement("li"); // ajouter la classe
+  newLine = document.createElement("li"); // ajouter la classe
   newLine.innerText = toDoEntry.value;
 
   increment += 1;
@@ -31,7 +34,7 @@ function ajouteTache() {
 
   newDiv.appendChild(newLine);
   // Ajout du bouton de mise à jour
-  const newButton = document.createElement("button"); // ajouter la classe
+  newButton = document.createElement("button"); // ajouter la classe
   //newButton.onclick = modifTache(aa)
   newButton.addEventListener("click", modifTache.bind(null, aa));
   newButton.innerText = "Modif";
@@ -55,7 +58,7 @@ function modifTache(id_Recup) {
   let ligneMaj = document.getElementById(id_Recup);
   ligneMaj.className += "en_Cours";
 
-  // Basculle du texte dans zone de saisie
+  // Bascule du texte dans zone de saisie
   console.log("line.texte : ", ligneMaj.innerHTML);
   document.getElementById("toDoEntry").value = ligneMaj.innerHTML;
 
@@ -68,6 +71,134 @@ function modifTache(id_Recup) {
 
 
   // Modification de l'état des boutons de Créat/maj
-  document.getElementById("modifie_Tache").disabled = false;
-  document.getElementById("ajoute_Tache").disabled = true;
+  document.getElementById("maj_Line").disabled = false;
+  document.getElementById("del_Line").disabled = false;
+  document.getElementById("new_Line").disabled = true;
+  console.log("top1");
+  
+  toDoEntry.focus();
+  id_Svg = id_Recup;
+    return id_Svg ;
+}
+
+// Ajouter une nouvelle ligne de tache to do :
+function newTache() {
+  // Suppression des blancs
+  toDoEntry.value = toDoEntry.value.trim();
+
+  if (toDoEntry.value == "") {
+    alert("Veuillez saisir du texte avant de valider");
+    toDoEntry.value = "";
+    toDoEntry.focus();
+    return;
+  }
+
+  // Récupération de la tache
+  console.log("Valeur Entrée : ", toDoEntry.value);
+  
+  // Création d'une nouvelle ligne
+  const newDiv = document.createElement("div"); // ajouter la classe
+  newDiv.classList += "to_Do_Div";
+  increment += 1;
+  newDiv.id = "div_" + increment;
+  // Ajout element de liste; et l'associer à la div
+  newLine = document.createElement("li"); // ajouter la classe
+  newLine.innerText = toDoEntry.value;
+  newLine.id = "li_" + increment;
+  newLine.classList += "to_Do_Li";
+
+
+  // Ajout du bouton de mise à jour
+  const newButton = document.createElement("button"); // ajouter la classe
+  newLine.id 
+  newButton.addEventListener("click", modifTache.bind(null, newLine.id));
+  newButton.innerText = "Modif";
+  newButton.id = "bt_" + increment;
+  newButton.classList += "to_Do_Button";
+
+ // Ajout du bouton et texte dans la div
+  newDiv.appendChild(newButton);
+  newDiv.appendChild(newLine);
+
+  // insert de la nouvelle ligne dans la liste préexistante dans html
+  const toDoList = document.getElementById("toDoList");
+  toDoList.appendChild(newDiv);
+
+  // RAZ zone d'entrée
+  toDoEntry.value = "";
+  toDoEntry.focus();
+}
+
+function majTache() {
+  console.log("id_Svg :", id_Svg);
+  // Suppression des blancs
+  toDoEntry.value = toDoEntry.value.trim();
+
+  if (toDoEntry.value == "") {
+    alert("Veuillez saisir du texte avant de valider");
+    toDoEntry.value = "";
+    toDoEntry.focus();
+    return;
+  }
+  // Modification du texte dans la liste
+  document.getElementById(id_Svg).innerHTML = toDoEntry.value;
+  // DéGriser tous les boutons clickables de la liste :
+  var myButton = document.querySelectorAll("button");
+  console.log("myButton : ", myButton);
+  for (var i = 0; i < myButton.length; i++) {
+    myButton[i].disabled = false;
+  }
+  // Modification de l'état des boutons de Créat/maj/supp
+  styleNewLine();
+
+}
+
+
+function delTache() {
+  console.log("id_Svg :", id_Svg);
+
+  // Suppression de la ligne dans la liste
+  document.getElementById(id_Svg).innerHTML = toDoEntry.value;
+
+  let bb  = id_Svg.match(/(\d+)/);
+  console.log("bb : ", bb[0])
+  let aa = "div_" + bb[0];
+  console.log("aa : ", aa)
+  newDiv = document.getElementById(aa);
+
+  // Supprime tous les enfant d'un élément
+  while (newDiv.firstChild) {
+    newDiv.removeChild(newDiv.firstChild);
+  }
+  styleNewLine()
+}
+
+////////////////////////////////////////////////////////
+// Modification de l'état des boutons de Créat/maj/supp
+////////////////////////////////////////////////////////
+function styleNewLine(){  // En cours nouvelle ligne 
+console.log("StyleNewLine ");
+  // DéGriser tous les boutons clickables de la liste :
+  var myButton = document.querySelectorAll("button");
+  for (var i = 0; i < myButton.length; i++) {
+    myButton[i].disabled = false;
+  }
+  document.getElementById("maj_Line").disabled = true;
+  document.getElementById("del_Line").disabled = true;
+ 
+  toDoEntry.value = "";
+  toDoEntry.focus();
+}
+
+
+function styleMajLine() { // En cours de mise à jour
+  var myButton = document.querySelectorAll("button");
+  console.log("StyleMajLine ");
+  for (var i = 0; i < myButton.length; i++) {
+    myButton[i].disabled = true;
+  }
+  document.getElementById("maj_Line").disabled = false;
+  document.getElementById("del_Line").disabled = false;
+  document.getElementById("new_Line").disabled = true;
+
 }
