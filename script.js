34d4e00beebd4ea2c84712a5e3be29bd6d7b9f18
MichelleVandes <m.vandeschrick@codeur.online>
2022-03-09 14:01:17 +0000

    fin provisoire exercice

--- script.js
+++ script.js
@@ -1,78 +1,10 @@
 // Initialisation liste :
 const toDoEntry = document.getElementById("toDoEntry");
 let increment = 0;
-let id_Svg, newList, newSpan, newButton, newDiv;
+let id_Svg, newList, newSpan, newButton, newDiv, myButton;
 
 document.getElementById("maj_Line").disabled = true;
 document.getElementById("del_Line").disabled = true;
-
-// Ajouter une nouvelle ligne de tache to do :
-function ajouteTache() {
-  // Suppression des blancs
-  toDoEntry.value = toDoEntry.value.trim();
-
-  if (toDoEntry.value == "") {
-    alert("Veuillez saisir du texte avant de valider");
-    toDoEntry.value = "";
-    toDoEntry.focus();
-    return;
-  }
-
-  // Récupération de la tache
-  // Création d'une nouvelle ligne
-  newList = document.createElement("li"); // ajouter la classe
-  newList.classList += "to_Do_Li";
-  // Ajout element de texte; et l'associer à la liste
-  newSpan = document.createElement("span"); // ajouter la classe
-  newSpan.innerText = toDoEntry.value;
-  newSpan.classList += "to_Do_Span";
-
-  increment += 1;
-  const aa = "ID_" + increment;
-  newSpan.id = aa;
-
-  newList.appendChild(newSpan);
-  // Bouton de mise à jour
-  newButton = document.createElement("button"); // ajouter la classe
-  newButton.addEventListener("click", modifTache.bind(null, aa));
-  newButton.classList += "to_Do_Button";
-  newButton.innerText = "Modif";
-  newList.appendChild(newButton);
-
-  // insert de la nouvelle ligne dans la liste préexistante dans html
-  const toDoList = document.getElementById("toDoList");
-  toDoList.appendChild(newList);
-
-  // RAZ zone d'entrée
-  toDoEntry.value = "";
-  toDoEntry.focus();
-}
-
-function modifTache(id_Recup) {
-  // Récupération de la tache via le focus, soit n°liste active
-  let spanMaj = document.getElementById(id_Recup);
-
-  spanMaj.classList.add("en_Cours");
-
-  // Bascule du texte dans zone de saisie
-  console.log("span.texte : ", spanMaj.innerHTML);
-  document.getElementById("toDoEntry").value = spanMaj.innerHTML;
-
-  // Griser tous les boutons clickables de la liste :
-  var myButton = document.querySelectorAll("button");
-  for (var i = 0; i < myButton.length; i++) {
-    myButton[i].disabled = true;
-  }
-
-  // Modification de l'état des boutons de Créat/maj
-  document.getElementById("maj_Line").disabled = false;
-  document.getElementById("del_Line").disabled = false;
-  document.getElementById("new_Line").disabled = true;
-
-  toDoEntry.focus();
-  id_Svg = id_Recup;
-  return id_Svg;
-}
 
 // Ajouter une nouvelle ligne de tache to do :
 function newTache() {
@@ -91,9 +23,9 @@ function newTache() {
   for (var i = 0; i < myImg.length; i++) {
     myImg[i].classList.add("display_None");
   }
-  document.getElementById("divToDo").classList.add("to_Do")
+  document.getElementById("divToDo").classList.add("to_Do");
   document.getElementById("stylo2").classList.remove("display_None");
- 
+
   // Récupération de la tache
 
   // Création d'une nouvelle ligne
@@ -110,7 +42,7 @@ function newTache() {
   // Ajout du bouton de mise à jour
   const newButton = document.createElement("button"); // ajouter la classe
   newSpan.id;
-  newButton.addEventListener("click", modifTache.bind(null, newSpan.id));
+  newButton.addEventListener("click", modifTacheEnCours.bind(null, newSpan.id));
   newButton.innerText = "Modif";
   newButton.id = "bt_" + increment;
   newButton.classList += "to_Do_Button";
@@ -171,8 +103,33 @@ function delTache() {
   aa = "li_" + bb[0];
   newList.remove(aa);
   styleNewLine();
+  affichagePlage(); 
 }
 
+function modifTacheEnCours(id_Recup) {
+  // Récupération de la tache via le focus, soit n°liste active
+  let spanMaj = document.getElementById(id_Recup);
+
+  spanMaj.classList.add("en_Cours");
+
+  // Bascule du texte dans zone de saisie
+  console.log("span.texte : ", spanMaj.innerHTML);
+  document.getElementById("toDoEntry").value = spanMaj.innerHTML;
+
+  // Griser tous les boutons clickables de la liste :
+  var myButton = document.querySelectorAll("button");
+  for (var i = 0; i < myButton.length; i++) {
+    myButton[i].disabled = true;
+  }
+
+  // Modification de l'état des boutons de Créat/maj
+  document.getElementById("maj_Line").disabled = false;
+  document.getElementById("del_Line").disabled = false;
+  document.getElementById("new_Line").disabled = true;
+  toDoEntry.focus();
+  id_Svg = id_Recup;
+  return id_Svg;
+}
 ////////////////////////////////////////////////////////
 // Modification de l'état des boutons de Créat/maj/supp
 ////////////////////////////////////////////////////////
@@ -188,25 +145,29 @@ function styleNewLine() {
 
   toDoEntry.value = "";
   toDoEntry.focus();
-
-  // Si aucune tâche à faire, Affichage de l'image Plage
-  if (myButton.length >= 3) {
-    let myImg = document.getElementsByClassName("plage");
-    for (var i = 0; i < myImg.length; i++) {
-      myImg[i].classList.remove("display_None");
-    }
-    document.getElementById("divToDo").classList.remove("to_Do");
-    document.getElementById("stylo2").classList.add("display_None");
-  }
 }
 
 function styleMajLine() {
   // En cours de mise à jour
-  var myButton = document.querySelectorAll("button");
+  myButton = document.querySelectorAll("button");
   for (var i = 0; i < myButton.length; i++) {
     myButton[i].disabled = true;
   }
   document.getElementById("maj_Line").disabled = false;
   document.getElementById("del_Line").disabled = false;
   document.getElementById("new_Line").disabled = true;
+}
+
+function affichagePlage() {
+  // Si aucune tâche à faire, Affichage de l'image Plage
+  myButton = document.querySelectorAll("button");
+  console.log("Nb Btn suite suppression ", myButton.length);
+  if ((myButton.length = 3)) {
+    let myImg = document.getElementsByClassName("plage");
+    for (var i = 0; i < myImg.length; i++) {
+      myImg[i].classList.remove("display_None");
+    }
+   // document.getElementById("divToDo").classList.remove("to_Do");
+    document.getElementById("stylo2").classList.add("display_None");
+  }
 }
